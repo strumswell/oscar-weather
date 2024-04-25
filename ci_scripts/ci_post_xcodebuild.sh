0f8135cf2f8f9c91f2a23033773e1e0aab3370b0
Philipp Bolte <philipp.bukkit@gmail.com>
2024-04-25 07:27:24 +0200

    chore: upload dsym to sentry in ci

---
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -0,0 +1,21 @@
+set -e
+
+if [ ! -d "$CI_ARCHIVE_PATH" ]; then
+    echo "Archive does not exist, skipping Sentry upload"
+    exit 0
+fi
+
+# This is necessary in order to have sentry-cli
+# install locally into the current directory
+export INSTALL_DIR=$PWD
+
+if [[ $(command -v sentry-cli) == "" ]]; then
+    echo "Installing Sentry CLI"
+    curl -sL https://sentry.io/get-cli/ | bash
+fi
+
+echo "Authenticate to Sentry"
+sentry-cli login --auth-token $SENTRY_AUTH_TOKEN
+
+echo "Uploading dSYM to Sentry"
+sentry-cli debug-files upload -o 'philipp-bolte' -p 'oscar' $CI_ARCHIVE_PATH
