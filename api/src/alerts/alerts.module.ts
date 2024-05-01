552265018714613550443d6dd594dbf09bd2ab86
Philipp Bolte <philipp.bolte@spherity.com>
2024-05-01 19:14:16 +0200

    chore(refactor): remove nestjs api

--- api/src/alerts/alerts.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AlertsService } from './alerts.service';
-import { AlertsController } from './alerts.controller';
-@Module({
-  imports: [],
-  controllers: [AlertsController],
-  providers: [AlertsService],
-})
-export class AlertsModule {}
