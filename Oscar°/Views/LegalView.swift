2370f4998bc52a480a0cd40f420a42d8ca9d1487
Philipp Bolte <philipp.bukkit@gmail.com>
2024-05-05 05:19:04 +0200

    <3
    Signed-off-by: Philipp Bolte <philipp.bukkit@gmail.com>

--- Oscar°/Views/LegalView.swift
+++ Oscar°/Views/LegalView.swift
@@ -12,7 +12,7 @@ struct LegalView: View {
     @Environment(\.presentationMode) var presentationMode
 
     var body: some View {
-        NavigationView {
+        NavigationStack {
             VStack {
                 Form {
                     Section(header: Text("Über")) {
@@ -155,28 +155,28 @@ struct LegalView: View {
                         }
                     }
                     
-                    HStack {
-                        Spacer()
-                        VStack {
-                            Text("Oscar° Weather")
-                                .font(.body)
-                                .bold()
-                            Text("by Philipp Bolte")
-                                .font(.caption)
-                                .padding(.bottom, 2)
-                            Text("In Gedenken an Kater Oscar von der Katzenfreiheit")
-                                .font(.caption2)
-                                .foregroundColor(.gray)
-                            Text("* 17.04.02 – † 03.08.21")
-                                .font(.caption2)
-                                .foregroundColor(.gray)
+                    NavigationLink {
+                        MemoryView()
+                            .navigationBarBackButtonHidden()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            VStack {
+                                Text("Oscar° Weather")
+                                    .font(.body)
+                                    .bold()
+                                Text("by Philipp Bolte")
+                                    .font(.caption)
+                                    .padding(.bottom, 2)
+                            }
+                            Spacer()
                         }
-                        Spacer()
+                        .padding(.bottom, 1)
                     }
-                    .padding(.bottom, 1)
+
                 }
             }
-            .navigationBarTitle(Text("Rechtliches"), displayMode: .inline)
+            .navigationTitle("Rechtliches")
             .toolbar(content: {
                 ToolbarItem(placement: .navigationBarTrailing, content: {
                     Button("Fertig", action: {
